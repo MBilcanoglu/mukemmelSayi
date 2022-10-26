cbefa16602e212d4101123aa257c6d05c0660d2b
MBilcanoglu <72490880+MBilcanoglu@users.noreply.github.com>
2022-10-27 01:38:23 +0300

    Add files via upload

---
+++ mukemmelSayi/src/mukemmelSayi/Main.java
@@ -0,0 +1,30 @@
+package mukemmelSayi;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number;
+		int total=0;
+		
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.print("Bir sayı giriniz: ");
+		number=scan.nextInt();
+		
+		for(int i=1; i<number; i++) {
+			if(number%i==0) {
+				total+=i;
+			}
+		}
+		
+		if(total==number) {
+			System.out.println(number+" Mükemmel sayıdır.");
+		}else {
+			System.out.println(number+" Mükemmel sayı değildir.");
+		}
+		
+	}
+
+}
